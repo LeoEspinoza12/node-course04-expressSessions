9b49a1dfad7a165d277f1eb3b1e6d1b022bb65ae
Leo espinoza <leo.spinoza@yahoo.com>
2018-08-29 14:13:02 -0500

    added modules

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// const createError = require('http-errors');
-// const express = require('express');
-// const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 
 // here we need the session because we have to 
